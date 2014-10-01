3de5bb99431290a913353933c78a4ec06542e5aa
Adan <adanasandoval@gmail.com>
2014-10-01 00:52:06 -0500

    initial commit. Functional Django poll site

---
+++ mysite/polls/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def __unicode__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.choice_text
